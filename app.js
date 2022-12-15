c9088524dc5b6c8aac3a336a16f873fe1f9df61d
Bolarg9764 <bolarg9764@gmail.com>
2022-12-15 08:37:29 -0500

    Update app.js
    edit user input

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 //window.alert('page loaded!');
 
+/*========= GLOBAL SCOPE =============*/
 /*========= VARIABLE DECLARATION =============*/
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
 function createLI(text) {
-      /*========= INSIDE FUNCTION
-      VARIABLE DECLARATION =============*/
+      /*========= FUNCTION SCOPE========*/
+      /*===VARIABLE DECLARATION ========*/
     const li = document.createElement('li');
+    const span = document.createElement('span');
     const label = document.createElement('label');
     const checkbox = document.createElement('input');
-    const button = document.createElement('button');
+    const editButton = document.createElement('button');
+    const removeButton = document.createElement('button');
 
-    li.textContent = text;
+    span.textContent = text;
+    li.appendChild(span);
     label.textContent = "confirmed";
 
     /* CHECKBOX */
     checkbox.type = "checkbox";
     label.appendChild(checkbox);
     li.appendChild(label);
-
+    
+    /*EDIT BUTTON  */
+    editButton.textContent = "edit";
+    li.appendChild(editButton);
+   
     /*REMOVE BUTTON  */
-    button.textContent = "remove";
-    li.appendChild(button);
+    removeButton.textContent = "remove";
+    li.appendChild(removeButton);
     return li;
+
 }
 
 form.addEventListener('submit', (e) => {
@@ -50,9 +59,20 @@ checked ? listItem.className = "responded" : listItem.className = " " ;
 
 ul.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
-        const li = e.target.parentNode;
+        const button = e.target;
+        const li = button.parentNode;
         const ul = li.parentNode;
-        ul.removeChild(li);
-    }   
+        if (button.textContent === 'remove') {
+            ul.removeChild(li);
+        } else if (button.textContent === 'edit') { 
+          const span = li.firstElementChild;
+          const input = document.createElement('input');
+          input.type = 'text';
+          input.value  = span.textContent;
+          li.insertBefore(input, span);
+          li.removeChild(span);
+         // li.textContent.replace(`${editButton}, ${removeButton}`);
+        }
+    } 
 });
 
